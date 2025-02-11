4c01046671b57565fdca1c64bef1a0246055d0ab
aminebenfraj <mohamedamine.benfredj@polytechnicien.tn>
2025-02-11 09:30:41 +0100

    vite and tailwind config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
-import ProductForm from "./components/product-form"
+import { Routes, Route } from 'react-router-dom';
+import ProductForm from './pages/product-form';
 
-export default function App() {
-  return <ProductForm />
+function App() {
+  return (
+    <Routes>
+      <Route path="/form" element={<ProductForm />} />
+    </Routes>
+  );
 }
 
+export default App;
