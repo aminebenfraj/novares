e6bbae48acaf07d28359e7e68c0126b9788bc8b8
aminebenfraj <mohamedamine.benfredj@polytechnicien.tn>
2025-02-12 08:27:42 +0100

    removed role from customer

--- backend/models/CustomerModel.js
+++ backend/models/CustomerModel.js
@@ -7,9 +7,7 @@ const customerSchema = new Schema(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     phone: { type: String, required: true, unique: true },
-    address: { type: String, required: true },
-    role: { type: String, default: "customer", enum: ["customer"] }, // Always "customer"
-  },
+    address: { type: String, required: true },  },
   { timestamps: true }
 );
 
