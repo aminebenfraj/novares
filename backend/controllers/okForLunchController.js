16c7fbbd0af63064b790cace33d8201fc93a39a9
aminebenfraj <mohamedamine.benfredj@polytechnicien.tn>
2025-04-07 11:03:14 +0200

    refactor: rename model file and update import path for OkForLunch

--- backend/controllers/okForLunchController.js
+++ backend/controllers/okForLunchController.js
@@ -1,4 +1,4 @@
-const OkForLunch = require("../models/Ok_for_lunch")
+const OkForLunch = require("../models/OkForLunch")
 const Checkin = require("../models/CheckInModel") // ✅ Ensure this is correctly imported
 const path = require("path")
 const fs = require("fs")
